c5bbb5197a76ed30422fdb042487cb3bc829d0f2
Nihalbagul <nihalbagul98@gmail.com>
2025-04-29 18:21:29 +0530

    first commit

---
+++ 01. Basics/app.js
@@ -0,0 +1,48 @@
+
+
+
+
+// // console.log('hello nihal')
+// // // salkdfhsadkfjashfaslfjjfasklfjklaskdff
+// // console.log('hello nihal')
+// //clg->console.log
+// //log->console.log
+
+// // Variables
+// // var, let, const
+
+// //(reserved words) (variable name) type of value
+// // var name = 'Nihal';
+// //not use much now var
+// // let age = "23";
+// // declare varialbe
+
+// // Assign value to variable 
+
+// // let bannana;
+
+// // bannana = 'this is bannana';
+// // console.log(age);
+// // console.log(bannana);
+// // const isCool = true;
+
+// // 1. Create variable name  (name) and store your name
+// let name = "Nihal Bagul";
+// console.log(name);
+
+// // 2. Create variable name (whatDoYouWannaBecomeInYourLife) and store "programmer"
+
+// let whatDoYouWannaBecomeInYourLife = "programmer";
+// console.log(whatDoYouWannaBecomeInYourLife);
+
+// // 3. Create variable name (gender) and store your gender.
+
+// let gender = "Male";
+// console.log(gender);
+
+// //4. Create Variable name (twitterHandle) and store your twitter handle.
+
+// let twitterHandle = "@NihalBagul";
+// console.log(twitterHandle);
+
+// //5. finally log all variable to the console.
