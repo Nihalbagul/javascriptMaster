f1b7cfc7e4a6fcde9b3870e079ca9327ff6ee107
Nihalbagul <nihalbagul98@gmail.com>
2025-05-01 17:41:30 +0530

    day3

---
+++ 01. Beginners/02. Primitives/05 Strings/livecoding/app.js
@@ -0,0 +1,50 @@
+//Strings
+
+'nihal'
+
+'this is the lots of the text'
+
+let firstName = '   Nihal       ';
+
+let lastName = 'Bagul';
+
+// 1. Concatenation
+
+// let fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+// 2. Append
+
+// firstName += " Pro Coder";
+ 
+// console.log(firstName);
+
+//3. Length
+// console.log(firstName.length);
+
+//4. Cases
+
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+
+//5. Slice
+// console.log(firstName.slice(0, 3));
+
+//6. Split & Join
+//  console.log(firstName.split(''));
+//  console.log(firstName.join(' '));
+
+//7. Includes
+
+// console.log(firstName.includes('N'));
+
+//8. Trim
+
+// console.log(firstName.trim());
+// console.log(firstName + ' ' + lastName); 
+
+// console.log(firstName);
+// console.log(lastName);
+
+let des = `My name is ${firstName} ${lastName}`;
+console.log(des);
---
+++ 01. Beginners/02. Primitives/06 Type Converstion/livecoding/app.js
@@ -0,0 +1,20 @@
+//Type Converstion
+
+let money = 100;
+// let food = '100';
+// let total = parseInt(money) ;
+// money = +money ;
+// money = Number(money);
+
+// -Converting number to String
+
+// money = money.toString();
+// money = String(money);
+
+// - Convert String to decilmal number
+
+// money = parseFloat(money);
+
+// money = Number(money);
+
+console.log(typeof money);
